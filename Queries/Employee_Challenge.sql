1d8eb3b6253fd6f23d2237c13697d1a315d924dd
mayajaral <35385165+mayajaral@users.noreply.github.com>
2020-12-29 11:58:26 +0530

    All challenge analysis

---
+++ Queries/Employee_Challenge.sql
@@ -0,0 +1,45 @@
+SELECT em.emp_no,
+	em.first_name,
+	em.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees AS em
+INNER JOIN titles AS ti
+ON (em.emp_no = ti.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY em.emp_no ASC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no ASC, to_date DESC;
+
+-- Use COUNT and GROUP BY to find retiring_titles
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+-- Mentorship Program
+SELECT DISTINCT ON(em.emp_no) em.emp_no, 
+	em.first_name,
+	em.last_name,
+	em.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibilty
+FROM employees AS em
+INNER JOIN dept_emp AS de
+ON (em.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (em.emp_no = ti.emp_no)
+WHERE (em.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY em.emp_no ASC;
